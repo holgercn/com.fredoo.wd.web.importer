6c2939f887f016f645dd5dea1b9549a74ad44d76
hw <holgercn@hotmail.com>
2013-12-19 23:00:34 +0100

    improvement

--- src/main/java/com/fredoo/wd/web/importer/App.java
+++ src/main/java/com/fredoo/wd/web/importer/App.java
@@ -19,29 +19,64 @@ public class App {
     connectionProps.put("user", "fredoo");
     connectionProps.put("password", "ai198052");
 
-    conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + database, connectionProps);
+    conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + database
+        + "?useUnicode=true&characterEncoding=UTF-8", connectionProps);
     System.out.println("Connected to database : " + database);
     return conn;
   }
 
   public static void main(String[] args) throws Exception {
     App app = new App();
-    Connection fredoo = app.getConnection("fredoo");
-    String insert = "insert into USER(id,email,nick_name,password,created,enabled) values (?,?,?,?,?,?)";
-    PreparedStatement statement_fredoo = (PreparedStatement) fredoo.prepareStatement(insert);
+    Connection fredooConn = app.getConnection("fredoo");
+    Connection oxwa123Conn = app.getConnection("oxwa123");
 
-    Connection oxwa123 = app.getConnection("oxwa123");
-    Statement statement = oxwa123.createStatement();
-    ResultSet rs = statement.executeQuery("select * from ow_base_user");
+    // handle user
+    Statement delete = fredooConn.createStatement();
+    delete.execute("delete from EVENT");
+    delete.execute("delete from USER");
+    
+    delete.close();
+
+    Statement oxwa123 = oxwa123Conn.createStatement();
+    ResultSet rs = oxwa123.executeQuery("select * from ow_base_user");
+    String insert = "insert into USER(id,email,nick_name,password,created,enabled) values (?,?,?,?,?,?)";
+    PreparedStatement fredoo = (PreparedStatement) fredooConn.prepareStatement(insert);
     while (rs.next()) {
-      System.out.println(rs.getInt("id") + " " + rs.getString("email"));
-      statement_fredoo.setInt(1, rs.getInt("id"));
-      statement_fredoo.setString(2, rs.getString("email"));
-      statement_fredoo.setString(3, rs.getString("username"));
-      statement_fredoo.setString(4, rs.getString("password"));
-      statement_fredoo.setDate(5, new Date(Long.parseLong(rs.getString("joinStamp") + "000")));
-      statement_fredoo.setBoolean(6, true);
-      statement_fredoo.execute();
+      System.out.println("User : " + rs.getInt("id") + " " + rs.getString("email"));
+      fredoo.setInt(1, rs.getInt("id"));
+      fredoo.setString(2, rs.getString("email"));
+      fredoo.setString(3, rs.getString("username"));
+      fredoo.setString(4, rs.getString("password"));
+      fredoo.setDate(5, new Date(Long.parseLong(rs.getString("joinStamp") + "000")));
+      fredoo.setBoolean(6, true);
+      fredoo.execute();
     }
+    rs.close();
+    oxwa123.close();
+    fredoo.close();
+
+    // handle event
+    oxwa123 = oxwa123Conn.createStatement();
+    rs = oxwa123.executeQuery("select * from ow_event_item");
+    insert = "insert into EVENT(id,created,description,end,image,location,start,status,title,type,owner_id) values (?,?,?,?,?,?,?,?,?,?,?)";
+    fredoo = (PreparedStatement) fredooConn.prepareStatement(insert);
+    while (rs.next()) {
+      System.out.println("Event : " + rs.getInt("id") + " " + rs.getString("title"));
+      fredoo.setInt(1, rs.getInt("id"));
+      fredoo.setDate(2, new Date(Long.parseLong(rs.getString("createTimeStamp") + "000")));
+      fredoo.setBlob(3, rs.getBlob("description"));
+      fredoo.setDate(4, new Date(Long.parseLong(rs.getString("endTimeStamp") + "000")));
+      fredoo.setString(5, rs.getString("image"));
+      fredoo.setString(6, rs.getString("location"));
+      fredoo.setDate(7, new Date(Long.parseLong(rs.getString("startTimeStamp") + "000")));
+      fredoo.setInt(8, rs.getInt("status"));
+      fredoo.setString(9, rs.getString("title"));
+      fredoo.setInt(10, 0);
+      fredoo.setInt(11, rs.getInt("userId"));
+      fredoo.execute();
+    }
+    rs.close();
+    oxwa123.close();
+    fredoo.close();
   }
 }
