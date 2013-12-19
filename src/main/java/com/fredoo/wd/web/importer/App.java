4054abf57c50659e9ef51e37fad8f9cac98c4fb4
hw <holgercn@hotmail.com>
2013-12-19 23:56:35 +0100

    add user name etc.

--- src/main/java/com/fredoo/wd/web/importer/App.java
+++ src/main/java/com/fredoo/wd/web/importer/App.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
 import java.util.Properties;
 
 import com.mysql.jdbc.PreparedStatement;
@@ -26,6 +27,8 @@ public class App {
   }
 
   public static void main(String[] args) throws Exception {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
     App app = new App();
     Connection fredooConn = app.getConnection("fredoo");
     Connection oxwa123Conn = app.getConnection("oxwa123");
@@ -34,22 +37,55 @@ public class App {
     Statement delete = fredooConn.createStatement();
     delete.execute("delete from EVENT");
     delete.execute("delete from USER");
-    
+
     delete.close();
 
     Statement oxwa123 = oxwa123Conn.createStatement();
-    ResultSet rs = oxwa123.executeQuery("select * from ow_base_user");
-    String insert = "insert into USER(id,email,nick_name,password,created,enabled) values (?,?,?,?,?,?)";
+
+    ResultSet rs = oxwa123.executeQuery("select * from ow_base_user JOIN ow_base_avatar ON userId = ow_base_user.id");
+    String insert = "insert into USER(id,email,nick_name,password,created,enabled,avatar,birthday,first_name,last_name,gender) values (?,?,?,?,?,?,?,?,?,?,?)";
     PreparedStatement fredoo = (PreparedStatement) fredooConn.prepareStatement(insert);
     while (rs.next()) {
-      System.out.println("User : " + rs.getInt("id") + " " + rs.getString("email"));
-      fredoo.setInt(1, rs.getInt("id"));
+      int id = rs.getInt("id");
+      System.out.println("User : " + id + " " + rs.getString("email"));
+      fredoo.setInt(1, id);
       fredoo.setString(2, rs.getString("email"));
       fredoo.setString(3, rs.getString("username"));
       fredoo.setString(4, rs.getString("password"));
       fredoo.setDate(5, new Date(Long.parseLong(rs.getString("joinStamp") + "000")));
       fredoo.setBoolean(6, true);
+      fredoo.setString(7, rs.getString("hash"));
+
+      Statement oxwa1232 = oxwa123Conn.createStatement();
+      ResultSet reSet = oxwa1232.executeQuery("select * from ow_base_question_data where userId = " + id);
+      Date birth = null;
+      int sex = 0;
+      String first_name = null;
+      String last_name = "";
+      while (reSet.next()) {
+        String question = reSet.getString("questionName");
+        if (question.equals("sex")) {
+          sex = reSet.getInt("intValue");
+        }
+
+        if (question.equals("birthdate")) {
+          java.util.Date d = sdf.parse(reSet.getString("dateValue"));
+          birth = new Date(d.getTime());
+        }
+
+        if (question.equals("realname")) {
+          first_name = reSet.getString("textValue");
+        }
+      }
+      reSet.close();
+      oxwa1232.close();
+
+      fredoo.setDate(8, birth);
+      fredoo.setString(9, first_name);
+      fredoo.setString(10, last_name);
+      fredoo.setString(11, "" + sex);
       fredoo.execute();
+
     }
     rs.close();
     oxwa123.close();
